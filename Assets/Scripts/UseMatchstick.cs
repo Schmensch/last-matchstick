9270c57d18f9bf398d586ccebd2fc798fbb4e518
Jonathan Zeller <jonathan_zeller@outlook.com>
2020-10-22 11:51:12 +0200

    Matchstick-behavior: if "E" is pressed, matchstick goes on for 10 Second

---
+++ Assets/Scripts/UseMatchstick.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class UseMatchstick : MonoBehaviour
+{
+    public int framesLit = 0;
+    public Light light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) {
+            framesLit = 600;
+        }
+
+        if (framesLit > 0)
+        {
+            GetComponent<Light>().enabled = true;
+            framesLit = framesLit - 1;
+        }
+        else
+        {
+            GetComponent<Light>().enabled = false;
+        }
+    }
+}
